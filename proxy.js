e50936f5d5cc666c4eb1e231f44d6150f0c5074d
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2025-01-02 13:28:09 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,4 +1,3 @@
-import http from "http";
 import https from "https";
 import sharp from "sharp";
 
@@ -28,6 +27,7 @@ function shouldCompress(req) {
   return true;
 }
 
+
 // Function to compress the image and send it directly in the response
 function compress(req, res, inputStream) {
   const format = req.params.webp ? "webp" : "jpeg";
@@ -48,25 +48,30 @@ function compress(req, res, inputStream) {
         sharpInstance.grayscale();
       }
 
-      // Process the image into a buffer with additional metadata
-      return sharpInstance
-        .toFormat(format, { quality: req.params.quality })
-        .toBuffer({ resolveWithObject: true }); // Get both the image data and metadata
-    })
-    .then(({ data: output, info }) => {
-      // Set the response headers
-      res.setHeader('content-type', `image/${format}`);
-      res.setHeader('content-length', info.size);
-      res.setHeader('x-original-size', req.params.originSize);
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-      res.status(200).send(output);
+      // Process the image into a buffer
+      sharpInstance.toFormat(format, { quality: req.params.quality }).toBuffer((err, output, info) => {
+        if (err || !info || res.headersSent) {
+          console.error('Compression error or headers already sent.');
+          return redirect(req, res);
+        }
+
+        // Set response headers
+        res.setHeader('content-type', `image/${format}`);
+        res.setHeader('content-length', info.size);
+        res.setHeader('x-original-size', req.params.originSize);
+        res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+
+        // Send the processed image
+        res.status(200).send(output);
+      });
     })
     .catch((err) => {
-      console.error('Compression error:', err.message);
-      res.statusCode = 500;
-      res.send('Failed to compress the image.');
+      console.error('Metadata error:', err.message);
+      redirect(req, res);
     });
 }
+
+
 
 
 
@@ -101,10 +106,7 @@ export function fetchImageAndHandle(req, res) {
     quality: parseInt(req.query.l, 10) || DEFAULT_QUALITY,
   };
 
-  // Select the correct client (http or https) based on the URL protocol
-  const client = req.params.url.startsWith("https") ? https : http;
-
-  client
+  https
     .get(req.params.url, (response) => {
       if (response.statusCode >= 400) {
         res.statusCode = response.statusCode;
